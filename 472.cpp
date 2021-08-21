a8b1217722addbed6a5883e1bbcc40bf42f95129
yashul117 <yashultyagi11@gmail.com>
2021-08-21 17:35:19 +0530

    Bit manipulation

---
+++ 472.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPosition(int N) {
+        // code here
+
+        if (N == 0) {
+            return -1;
+        }
+
+        int pos = 0;
+        int count = 0;
+
+        while (N) {
+            if (N & 1) {
+                count++;
+
+                if (count > 1) {
+                    return -1;
+                }
+            }
+            pos++;
+            // cout<<"POS: "<<pos<<"\n";
+            N = N >> 1;
+
+        }
+
+        return pos;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin >> N;
+
+        Solution ob;
+        cout << ob.findPosition(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
